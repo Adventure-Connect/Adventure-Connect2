d2e2cfff23376f11a7bf051ceff926593069c44a
Renee Toscan <renee.toscan@outlook.com>
2023-07-29 11:36:24 -0700

    Change Password Styled and Navbar logo centered

--- client/components/ChangePassword.jsx
+++ client/components/ChangePassword.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import '../styles/ChangePassword.css'
 
 const ChangePassword = () => {
 
   return (
-    <>
+    <div className="change-password">
       <h1>Change Your Password</h1>
       <p>Password must be at least 6 characters including one special chracter</p>
       <label htmlFor="Current Password"></label>
-      <input type="text" name="currentPassword" id="currentPassword" />
+      <input type="text" name="currentPassword" id="currentPassword" placeholder="Current Password" />
       <label htmlFor="New Password"></label>
-      <input type="text" name="newPassword" id="newPassword" />
+      <input type="text" name="newPassword" id="newPassword" placeholder="New Password"/>
       <label htmlFor="Confirm New Password"></label>
-      <input type="text" name="confirmPassword" id="confirmPassword" />
+      <input type="text" name="confirmPassword" id="confirmPassword" placeholder="Retype New Password" />
 
-      <input type="submit" value="Submit" />
-    </>
+      <button>Change Password</button>
+    </div>
   )
 }
 
